865d7f2c3144c097795489b569d668642d13dfdc
Ashley Pittman <ashley.m.pittman@intel.com>
2022-08-09 09:13:33 +0100

    DAOS-7821 gha: Remove pylint from checks.
    This should all be handled via github actions now, so remove
    from Jenkins.
    
    Signed-off-by: Ashley Pittman <ashley.m.pittman@intel.com>

--- jenkins_github_checkwarn.sh
+++ jenkins_github_checkwarn.sh
@@ -17,7 +17,6 @@ checkpatch_py="$(find -L "${mydir}" -name $checkpatch_py -print -quit)"
 check_make="$(find -L "${mydir}" -name check_make_output.sh -print -quit)"
 check_style="$(find -L "${mydir}" -name checkpatch.pl -print -quit)"
 check_shell="$(find -L "${mydir}" -name shellcheck_scripts.sh -print -quit)"
-check_python="$(find -L "${mydir}" -name check_python.sh -print -quit)"
 
 # For a matrix review we only want to default for the full check for the
 # el7 target, just compiler warnings for the rest.
@@ -34,7 +33,7 @@ set -u
 # colon separated list
 def_checkpatch_paths="${check_make}"
 if [ "${FULL_REVIEW}" -ne 0 ]; then
-  def_checkpatch_paths+=":${check_style}:${check_shell}:${check_python}"
+  def_checkpatch_paths+=":${check_style}:${check_shell}"
 fi
 
 : "${CHECKPATCH_PATHS:="${def_checkpatch_paths}"}"
@@ -60,32 +59,6 @@ export REVIEW_HISTORY_PATH="${REVIEW_HISTORY_BASE}"/REVIEW_HISTORY
 if [ ! -e "$REVIEW_HISTORY_PATH" ]; then
   mkdir -p "${REVIEW_HISTORY_BASE}"
   touch "${REVIEW_HISTORY_PATH}"
-fi
-
-# CentOS PyOpenSSL out of date, need a virtualenv to use correct one.
-
-if [ -n "${GERRIT_PROJECT-}" ]; then
-  # Some prompt variables are usually not set.
-  set +u
-  if [ -n "${WORKSPACE}" ];then
-    # Need to remove older virtualenv with pip/wheel etc.
-    if grep "\\#\\!${WORKSPACE}"  -r test_env; then
-      rm -rf test_env
-    fi
-  fi
-  if [ ! -e test_env ]; then
-    virtualenv --system-site-packages \
-      --no-setuptools --no-pip --no-wheel test_env
-    # shellcheck disable=SC1091
-    source test_env/bin/activate
-    pip install -I --root test_env --prefix test_env \
-      -U --force-reinstall pyOpenSSL
-    pip install -I --root test_env --prefix test_env -U pylint flake8
-  else
-    # shellcheck disable=SC1091
-    source test_env/bin/activate
-  fi
-  set -u
 fi
 
 if [ -f ./ci/patch_src_in_place ]
